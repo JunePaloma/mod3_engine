ee1a4276087ce62477b2db025a04be02aafd09d3
JunePaloma <kborkoski@gmail.com>
2017-11-28 16:21:20 -0700

    Added controller for item invoice relationship and the test seems to be passing

--- spec/requests/api/v1/items/item_invoice_items_spec.rb
+++ spec/requests/api/v1/items/item_invoice_items_spec.rb
@@ -1,13 +1,14 @@
 require "rails_helper"
 
 describe "Item invoice items relationship API" do
-  describe "GET #show" do
+  describe "GET #index" do
     it "returns invoice items for an item" do
        item = create(:item)
        invoice_items = create_list(:invoice_item, 2, item: item)
        get "/api/v1/items/#{item.id}/invoice_items"
        parsed_body = JSON.parse(response.body, symbolize_names: true)
 
+       expect(response).to be_success
        expect(parsed_body.count).to eq(2)
        expect(parsed_body.first[:id]).to eq(invoice_items.first.id)
       #  expect(parsed_body).to eq("#{item.invoice_items}")
