c0121bbd9b143e496fc5d83a552942ab82e30946
JunePaloma <kborkoski@gmail.com>
2017-11-28 14:30:17 -0700

    Test passing for find all items

--- spec/requests/items/search_spec.rb
+++ spec/requests/items/search_spec.rb
@@ -9,4 +9,12 @@ describe "Item Search API" do
        expect(parsed_body).to eq({id: item.id, name: item.name})
     end
   end
+  describe "GET #index" do
+    it "returns all items" do
+       item = create(:item)
+       get "/api/v1/items/find_all?name=#{item.name}"
+       parsed_body = JSON.parse(response.body, symbolize_names: true)
+       expect(parsed_body).to eq([{id: item.id, name: item.name}])
+    end
+  end
 end
