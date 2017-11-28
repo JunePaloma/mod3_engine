006ec80f2ed7309181128b281f39c3bd6f37ce4a
JunePaloma <kborkoski@gmail.com>
2017-11-27 21:17:41 -0700

    test passing for merchant show

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -12,4 +12,13 @@ describe "Merchants API" do
     merchants = JSON.parse(response.body)
     expect(merchants.count).to eq(3)
   end
+  it "can locate one merchant by id" do
+    id = create(:merchant).id
+    get "/api/v1/merchants/#{id}"
+    merchant = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(merchant["id"]).to eq(id)
+  end
+
 end
