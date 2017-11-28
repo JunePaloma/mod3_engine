ce629c052bf88c7c4584840dca642582920bcfb3
JunePaloma <kborkoski@gmail.com>
2017-11-27 21:24:58 -0700

    add test for customer records end point

---
+++ spec/requests/api/v1/customer_request_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+describe "Customer API" do
+  it "displays a list of all the merchants" do
+    create_list(:customer, 3)
+    get '/api/v1/customers'
+    customerss = JSON.parse(response.body)
+    expect(response).to be_success
+    expect(customers.count).to eq(3)
+  end
+  it "can locate one merchant by id" do
+    id = create(:customer).id
+    get "/api/v1/customers/#{id}"
+    customer = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(customer["id"]).to eq(id)
+  end
+
+end
