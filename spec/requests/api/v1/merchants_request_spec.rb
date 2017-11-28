67cf913d23f5424208631d7ab3fb53f2d916b4d7
JunePaloma <kborkoski@gmail.com>
2017-11-27 21:08:21 -0700

    initial records tests passing for merchants

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -1,11 +1,15 @@
 require 'rails_helper'
 
 describe "Merchants API" do
-  it "displays a list of all the merchants" do
+  it "is wired together properly" do
     create_list(:merchant, 3)
-
     get '/api/v1/merchants'
-
     expect(response).to be_success
   end
-end 
+  it "displays a list of all the merchants" do
+    create_list(:merchant, 3)
+    get '/api/v1/merchants'
+    merchants = JSON.parse(response.body)
+    expect(merchants.count).to eq(3)
+  end
+end
