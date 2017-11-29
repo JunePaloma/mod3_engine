266a9e209c4db2da93d6e28d27e72e5b297b6f73
JunePaloma <kborkoski@gmail.com>
2017-11-28 18:55:58 -0700

    Add more robust tests to customer search spec

--- spec/requests/customers/search_spec.rb
+++ spec/requests/customers/search_spec.rb
@@ -4,17 +4,21 @@ describe "Customer search API" do
   describe "GET #show" do
     it "returns a single customer" do
       customer = create(:customer)
-      get "/api/v1/customers/find?last_name=#{customer.last_name}"
+      create_list(:customer, 2)
+      get "/api/v1/customers/find?id=#{customer.id}"
       parsed_body = JSON.parse(response.body, symbolize_names: true)
       expect(parsed_body).to eq({id: customer.id, first_name: customer.first_name, last_name: customer.last_name})
+      expect(parsed_body.count).to eq(3)
     end
   end
   describe "GET #index" do
     it "returns all customers" do
        customer = create(:customer)
-       get "/api/v1/customers/find_all?id=#{customer.id}"
+       customer2 = create(:customer)
+       get "/api/v1/customers/find_all?last_name=#{customer.last_name}"
        parsed_body = JSON.parse(response.body, symbolize_names: true)
-       expect(parsed_body).to eq([{id: customer.id, first_name: customer.first_name, last_name: customer.last_name }])
+       expect(parsed_body.count).to eq(2)
+       expect(parsed_body).to eq([{id: customer.id, first_name: customer.first_name, last_name: customer.last_name }, {id: customer2.id, first_name: customer2.first_name, last_name: customer2.last_name}])
     end
   end
 end
